248304d873ed9c4a76af34ef09ad4f8507641aa5
Kamil Domański <kamil@domanski.co>
2023-12-11 12:20:53 +0100

    extrusion multiplier for ABS

--- set_opt_in_section.sh
+++ set_opt_in_section.sh
@@ -24,5 +24,19 @@ find_opt_in_section() {
     echo $((SECT_LINE + LINE_AFTER_SECT - 1))
 }
 
+is_line_empty() {
+    local LINE_NUMBER="$1"
+    local LINE="$(sed "$LINE_NUMBER!d" "$FILE")"
+    if [ -z "$LINE" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 OPT_LINE="$(find_opt_in_section "$SECTION" "$OPTION")"
+
+if is_line_empty "$OPT_LINE"; then
+    VALUE+="\n"
+fi
 (head -n $((OPT_LINE - 1)) "$FILE"; echo "$OPTION = $VALUE"; tail -n +$((OPT_LINE + 1)) "$FILE") > "$TMP_FILE" && mv "$TMP_FILE" "$FILE"
